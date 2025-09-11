ccd3ffaa38c830d07a0dcc5ebb74807319ef4b14
vaquita <cristhianvm55@gmail.com>
2025-09-11 11:36:22 -0500

    [SHIP] config lint and format

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>SISTEMA DE GESTION DE HISTORIAS CLINICAS</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>SISTEMA DE GESTION DE HISTORIAS CLINICAS</h1>
+        </>
+    );
 }
 
 export default App;
