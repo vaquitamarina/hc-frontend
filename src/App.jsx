a69466824353021b4b064d597fd4bdeb414d1dcf
vaquita <cristhianvm55@gmail.com>
2025-09-14 16:42:23 -0500

    [SHIP] add package lucide-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { CameraOff } from 'lucide-react';
+
 function App() {
   return (
     <>
       <h1>SISTEMA DE GESTION DE HISTORIAS CLINICAS</h1>
+      <CameraOff color="#990101" size={48} />
     </>
   );
 }
