b4d4ad6153fd3dc58b95835c1e0cb8e14648a35e
vaquita <cristhianvm55@gmail.com>
2025-09-27 08:59:16 -0500

    fix paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/hc-frontend/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@cm': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+    },
+  },
 });
