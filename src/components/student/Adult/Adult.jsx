a5dd4e13ded17ed66fbf6c5b316dcdc5c2e7d119
vaquita <cristhianvm55@gmail.com>
2025-09-27 01:56:04 -0500

    add adult menu

---
+++ src/components/student/Adult/Adult.jsx
@@ -0,0 +1,13 @@
+import 'Adult.css';
+import Button from '../../ui/Button/Button';
+
+function Adult() {
+  return (
+    <div className="adult">
+      <h2>Adult</h2>
+      <Button>Crear</Button>
+    </div>
+  );
+}
+
+export default Adult;
