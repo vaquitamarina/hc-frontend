bd5325576751e07dd486d07ae55e6c8586c128d1
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2025-12-03 23:25:09 -0500

    fix protected routes

--- src/components/routes/ProtectedRoutes.jsx
+++ src/components/routes/ProtectedRoutes.jsx
@@ -2,8 +2,13 @@ import { Navigate, Outlet } from 'react-router';
 import { useCurrentUser } from '@hooks/useAuth';
 
 function ProtectedRoutes() {
-  const { isError } = useCurrentUser();
-  if (isError) {
+  const { data: user, isLoading } = useCurrentUser();
+  
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+  
+  if (!user) {
     return <Navigate to="/login" replace />;
   }
 
