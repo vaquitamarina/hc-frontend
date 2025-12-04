ccf9cb64eed05da65b795c38011cf36ae080585a
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2025-12-03 19:49:02 -0500

    undo testline

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex justify-between items-center px-16 py-6 bg-[var(--color-primary)] !text-[var(--color-white)] shadow-[0_2px_4px_rgba(0,0,0,0.1)] top-0 left-0 w-full h-24 flex-shrink-0">
       <div className="flex items-center gap-4 flex-row">
         <img src="/logo1.png" alt="Logo" className="h-12 w-auto" />
-        <div className="font-bold text-2xl">UNjjJBG</div>
+        <div className="font-bold text-2xl">UNJBG</div>
         <div className="text-center text-base leading-tight flex-1 border-l-[2.5px] border-[#aaa] pl-5 pt-2.5">
           CLÍNICA ODONTOLÓGICA
           <br />
